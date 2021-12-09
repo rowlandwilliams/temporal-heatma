c64cb2f3bb777abfd36bb013866b3ce9e49a47a6
rowlandwilliams <rowland.williams.94@gmail.com>
2021-12-09 13:12:49 +0000

    update eslint config settings

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
 module.exports = {
-  reactStrictMode: true,
-}
+    future: {
+        webpack5: true,
+    },
+    webpack: (config, { dev }) => {
+        if (dev)
+            config.plugins.push(
+                new LiveReloadPlugin({
+                    port: 2999,
+                    hostname: 'localhost',
+                }),
+
+                new ESLintPlugin({
+                    extensions: ['ts', 'tsx'],
+                    failOnError: true,
+                }),
+            );
+
+        return config;
+    },
+};
