856b406bad1136c1e4ceb6e612f9a9149e62ee14
rowlandwilliams <rowland.williams.94@gmail.com>
2021-12-11 12:55:17 +0000

    configure ts

--- store/store.ts
+++ store/store.ts
@@ -3,7 +3,6 @@ import create from 'zustand';
 interface AppState {
     sampleState: string;
     setSampleState: (sampleState: string) => void;
-    suh: string;
 }
 
 export const useStore = create<AppState>((set) => ({
