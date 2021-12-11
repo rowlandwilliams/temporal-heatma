5e145e6fafc6dc831a5547ee8bcd2eae9f7a460f
rowlandwilliams <rowland.williams.94@gmail.com>
2021-12-11 12:49:19 +0000

    add script to wathc ts

--- store/store.ts
+++ store/store.ts
@@ -1,38 +1,12 @@
 import create from 'zustand';
-import { LinkTooltip, NodeTooltip } from '../types/types';
 
-interface SankeyState {
-    nodeIsHovered: boolean;
-    setNodeIsHovered: (nodeIsHovered: boolean) => void;
-    linkIsHovered: boolean;
-    setLinkIsHovered: (nodeIsHovered: boolean) => void;
-    nodeTooltipData: NodeTooltip;
-    setNodeTooltipData: (nodeTooltipData: NodeTooltip) => void;
-    linkTooltipData: LinkTooltip;
-    setLinkTooltipData: (linkTooltipData: LinkTooltip) => void;
-    activeNodes: string[];
-    addActiveNode: (node: string) => void;
-    removeActiveNode: (node: string) => void;
+interface AppState {
+    sampleState: string;
+    setSampleState: (sampleState: string) => void;
+    suh: string;
 }
 
-export const useStore = create<SankeyState>((set) => ({
-    nodeIsHovered: false,
-    setNodeIsHovered: (nodeIsHovered: boolean) => set({ nodeIsHovered }),
-    linkIsHovered: false,
-    setLinkIsHovered: (linkIsHovered: boolean) => set({ linkIsHovered }),
-    nodeTooltipData: {} as NodeTooltip,
-    setNodeTooltipData: (nodeTooltipData: NodeTooltip) => set({ nodeTooltipData }),
-    linkTooltipData: {} as LinkTooltip,
-    setLinkTooltipData: (linkTooltipData: LinkTooltip) => set({ linkTooltipData }),
-    activeNodes: [],
-    addActiveNode: (node: string) => {
-        set((state) => ({
-            activeNodes: [...state.activeNodes, node],
-        }));
-    },
-    removeActiveNode: (node: string) => {
-        set((state) => ({
-            activeNodes: state.activeNodes.filter((nodeName) => nodeName !== node),
-        }));
-    },
+export const useStore = create<AppState>((set) => ({
+    sampleState: 'suh',
+    setSampleState: (sampleState: string) => set({ sampleState }),
 }));
